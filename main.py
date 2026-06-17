f1e591968160bf8e731cd417681f4dcfabe3c25c
Anton Olguin <antonolguin1@gmail.com>
2026-06-17 10:05:46 -0600

    set up paddles

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
+# Set up main screen
 screen = Screen()
 screen.bgcolor("black")
 screen.title("Pong")
 screen.setup(800, 600)
+screen.tracer(0)
+
+# Set up paddles
+left_paddle = Paddle()
+right_paddle = Paddle()
+
+left_paddle.goto(-350, 0)
+right_paddle.goto(350, 0)
+screen.update()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -3,3 +3,10 @@ from turtle import Turtle
 class Paddle(Turtle):
     def __init__(self):
         super().__init__()
+        self.setup()
+
+    def setup(self):
+        self.penup()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
