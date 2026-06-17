43443c01aafccf69ce38a4b0719e35bfa0702bf0
Anton Olguin <antonolguin1@gmail.com>
2026-06-17 10:34:29 -0600

    add key events to move paddles

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from turtle import Turtle, Screen
 from paddle import Paddle
 
@@ -15,5 +16,16 @@ right_paddle = Paddle()
 left_paddle.goto(-350, 0)
 right_paddle.goto(350, 0)
 screen.update()
+
+# Set up events
+screen.listen()
+screen.onkeypress(key="w", fun=left_paddle.go_up)
+screen.onkeypress(key="s", fun=left_paddle.go_down)
+screen.onkeypress(key="Up", fun=right_paddle.go_up)
+screen.onkeypress(key="Down", fun=right_paddle.go_down)
+
+while True:
+    screen.update()
+    time.sleep(0.016)
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -10,3 +10,11 @@ class Paddle(Turtle):
         self.shape("square")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.color("white")
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
